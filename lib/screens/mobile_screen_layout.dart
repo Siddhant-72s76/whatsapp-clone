8a09c1e2fae3abe470e7043d1d6f7318a4418cb9
Siddhant <siddhant04s05@gmail.com>
2023-11-02 23:54:51 +0530

    Contact List added

--- lib/screens/mobile_screen_layout.dart
+++ lib/screens/mobile_screen_layout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:whats_app_clone/colors.dart';
+import 'package:whats_app_clone/widgets/contacts_list.dart';
 
 class MobileScreenLayout extends StatelessWidget {
   const MobileScreenLayout({super.key});
@@ -10,6 +11,8 @@ class MobileScreenLayout extends StatelessWidget {
       length: 3,
       child: Scaffold(
         appBar: AppBar(
+          backgroundColor: appBarColor,
+          elevation: 0,
           title: const Text(
             'Whats App',
             style: TextStyle(
@@ -55,7 +58,7 @@ class MobileScreenLayout extends StatelessWidget {
             ],
           ),
         ),
-        body: Center(child: Text('Mobile')),
+        body: const ContactsList(),
       ),
     );
   }
